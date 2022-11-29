a9ebe5fb3ad102c9c1f51b813bf1e136d6aedb38
PashaDahulych <112983354+PashaDahulych@users.noreply.github.com>
2022-11-29 11:25:33 +0100

    Add files via upload

---
+++ js/screipt.js
@@ -0,0 +1,61 @@
+/* Завдання 1 */
+// Строгий режим
+"use strict"
+console.log( alert (`Обережно, файлик JS робив новачок. Краще прийняти засокійливе =)`) );
+// Який варіант запису вірний?
+console.log( 'Вчимо JS' );
+
+/*console.log( 'Вчимо JS' );
+
+console.log( 'Вчимо' )
+console.log( 'JS' )
+
+
+console.log( 'Вчимо' ) console.log( 'JS' )
+
+console.log( 'Вчимо );
+console.log( 'JS' );*/
+
+
+/* Завдання 2 */
+
+let userEyesColor;
+let user, userName;
+userName = 'Вася';
+user = userName;
+console.log( user );
+
+
+/*let user = 'Вася' let age = 36
+*/
+
+// let myage = 36;
+
+/*const BLOCKSIZE = 14 + 36;
+console.log( BLOCKSIZE );*/
+
+/*const BLOCKSIZE = 100;
+console.log( BLOCKSIZE );*/
+
+if ( true ) {
+	var size = 15;
+}
+console.log( size );
+
+/* Завдання 3 */
+
+let userAge = 26;
+let userInfo = "Паша ${userAge}";
+// console.log( userInfo );
+
+/*let userHeight = 175 / 0;
+// Вернеться значення NaN
+console.log( Height );*/
+
+let useName;
+// Вернеться значення Null
+console.log( typeof userName );
+
+let userSize = "45" / "5";
+// Вернеться значення Number
+console.log( typeof userSize );
